635d8f821b2acd0a60e87883089adecf09a36657
Sumith Wickramasinghe <sumithkck89@gmail.com>
2021-10-26 23:00:24 +0530

    Added functionality with JavaScript

--- index.js
+++ index.js
@@ -15,6 +15,14 @@ document.querySelector("#five").addEventListener("click", function() {
     billAmount = document.getElementById('bill-amount').value;
     numberOfPeople = document.getElementById('num-of-people').value;
 
+    var tipAmount = (billAmount * 5) / 100;
+    var tipPerPerson = tipAmount / numberOfPeople;
+    var totalPerPerson = (billAmount * (105) / 100) / numberOfPeople;
+
+    document.querySelector("#tip-display-01").innerHTML = tipPerPerson;
+    document.querySelector("#tip-display-02").innerHTML = totalPerPerson;
+
+
 
 });
 
@@ -28,6 +36,14 @@ document.querySelector("#ten").addEventListener("click", function() {
 
     billAmount = document.getElementById('bill-amount').value;
     numberOfPeople = document.getElementById('num-of-people').value;
+
+
+    var tipAmount = (billAmount * 10) / 100;
+    var tipPerPerson = tipAmount / numberOfPeople;
+    var totalPerPerson = (billAmount * (110) / 100) / numberOfPeople;
+
+    document.querySelector("#tip-display-01").innerHTML = tipPerPerson;
+    document.querySelector("#tip-display-02").innerHTML = totalPerPerson;
 });
 
 document.querySelector("#fifteen").addEventListener("click", function() {
@@ -40,6 +56,14 @@ document.querySelector("#fifteen").addEventListener("click", function() {
 
     billAmount = document.getElementById('bill-amount').value;
     numberOfPeople = document.getElementById('num-of-people').value;
+
+
+    var tipAmount = (billAmount * 15) / 100;
+    var tipPerPerson = tipAmount / numberOfPeople;
+    var totalPerPerson = (billAmount * (115) / 100) / numberOfPeople;
+
+    document.querySelector("#tip-display-01").innerHTML = tipPerPerson;
+    document.querySelector("#tip-display-02").innerHTML = totalPerPerson;
 });
 
 document.querySelector("#twenty-five").addEventListener("click", function() {
@@ -52,6 +76,14 @@ document.querySelector("#twenty-five").addEventListener("click", function() {
 
     billAmount = document.getElementById('bill-amount').value;
     numberOfPeople = document.getElementById('num-of-people').value;
+
+
+    var tipAmount = (billAmount * 25) / 100;
+    var tipPerPerson = tipAmount / numberOfPeople;
+    var totalPerPerson = (billAmount * (125) / 100) / numberOfPeople;
+
+    document.querySelector("#tip-display-01").innerHTML = tipPerPerson;
+    document.querySelector("#tip-display-02").innerHTML = totalPerPerson;
 });
 
 document.querySelector("#fifty").addEventListener("click", function() {
@@ -64,9 +96,18 @@ document.querySelector("#fifty").addEventListener("click", function() {
 
     billAmount = document.getElementById('bill-amount').value;
     numberOfPeople = document.getElementById('num-of-people').value;
+
+
+    var tipAmount = (billAmount * 50) / 100;
+    var tipPerPerson = tipAmount / numberOfPeople;
+    var totalPerPerson = (billAmount * (150) / 100) / numberOfPeople;
+
+    document.querySelector("#tip-display-01").innerHTML = tipPerPerson;
+    document.querySelector("#tip-display-02").innerHTML = totalPerPerson;
 });
 
 document.querySelector("#custom").addEventListener("click", function() {
+    var percentage = prompt("Enter custom tip percentage(%): ");
     document.querySelector("#custom").classList.add("btn-selected");
     document.querySelector("#five").classList.remove("btn-selected");
     document.querySelector("#ten").classList.remove("btn-selected");
@@ -76,6 +117,15 @@ document.querySelector("#custom").addEventListener("click", function() {
 
     billAmount = document.getElementById('bill-amount').value;
     numberOfPeople = document.getElementById('num-of-people').value;
+
+
+    var tipAmount = (billAmount * percentage) / 100;
+    var tipPerPerson = tipAmount / numberOfPeople;
+    var temp = 1 + (percentage / 100);
+    var totalPerPerson = (billAmount * temp) / numberOfPeople;
+
+    document.querySelector("#tip-display-01").innerHTML = tipPerPerson;
+    document.querySelector("#tip-display-02").innerHTML = totalPerPerson;
 });
 
 document.querySelector("#reset").addEventListener("click", function() {
@@ -85,4 +135,7 @@ document.querySelector("#reset").addEventListener("click", function() {
     document.querySelector("#fifteen").classList.remove("btn-selected");
     document.querySelector("#twenty-five").classList.remove("btn-selected");
     document.querySelector("#fifty").classList.remove("btn-selected");
+
+    document.querySelector("#tip-display-01").innerHTML = "0.00";
+    document.querySelector("#tip-display-02").innerHTML = "0.00";
 });
